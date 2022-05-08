d72d77fb2fa8cacb95fb3129b8a79d07ac43d806
conghuy179 <105105177+conghuy179@users.noreply.github.com>
2022-05-08 18:36:11 +0700

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,17 @@
+< script >
+
+    function check() {
+
+        var name = document.getElementById("name");
+
+        var email = document.getElementById("email");
+        var title = document.getElementById("title");
+        var content = document.getElementById("content");
+
+
+    }
+
+}
+
+<
+/script>
---
+++ order.js
@@ -0,0 +1,35 @@
+< form action = "" >
+
+    <
+    label
+for = "name" > Name < /label>
+
+<
+input type = "text"
+name = "name"
+id = "name" > < br >
+
+    <
+    label
+for = "email" > Email < /label>
+
+<
+input type = "email"
+name = "email"
+id = "email" > < br >
+
+    <
+    label
+for = "birth" > Birth < /label>
+
+<
+input type = "date"
+name = "birth"
+id = "birth" > < br >
+
+    <
+    input type = "submit"
+value = "Click Here" >
+
+    <
+    /form>
